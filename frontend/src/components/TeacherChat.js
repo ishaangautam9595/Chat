18cf6bd4d3b30027e9d2272c586b4cde572ca7c3
Ishaan <ishaan@Ishaans-Laptop.local>
2025-07-24 10:40:19 +0530

    close chat

--- frontend/src/components/TeacherChat.js
+++ frontend/src/components/TeacherChat.js
@@ -200,13 +200,13 @@ const TeacherChat = ({ schoolName, teacherName, setSchoolName, setTeacherName })
         >
           {isSending ? 'Sending...' : 'Send'}
         </button>
-        <button
+        {/* <button
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 disabled:bg-gray-400"
           onClick={handleCloseChat}
           disabled={isSending}
         >
           {isSending ? 'Closing...' : 'Close Chat'}
-        </button>
+        </button> */}
       </div>
     </div>
   );
